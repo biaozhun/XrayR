ad9cd070880b62c6ccfa6a5a8dc0ae85d7650a14
biaozhun <qq410227037@live.com>
2022-07-06 16:42:15 +0800

    Update inboundbuilder_test.go

--- service/controller/inboundbuilder_test.go
+++ service/controller/inboundbuilder_test.go
@@ -3,8 +3,8 @@ package controller_test
 import (
 	"testing"
 
-	"github.com/Misaka-blog/XrayR/api"
-	. "github.com/Misaka-blog/XrayR/service/controller"
+	"github.com/biaozhun/XrayR/api"
+	. "github.com/biaozhun/XrayR/service/controller"
 )
 
 func TestBuildV2ray(t *testing.T) {
